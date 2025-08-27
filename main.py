ee0527c238bfe113c49c366c9dc0ae8c51096ba3
JenabPourya <alizadepourya@gmail.com>
2025-08-27 23:25:29 +0330

    Minor corrections again

--- main.py
+++ main.py
@@ -1,44 +1,44 @@
 # todo.py
 
-todo_list = []
+ToDo_list = []
 
-def show_menu():
+def Show_menu():
     print("\n To-Do List 📋")
     print("1. Show tasks")
     print("2. Add a task")
     print("3. Delete some tasks")
     print("4. Quit")
 
-def show_tasks():
-    if not todo_list:
-        print("Ooh...The list is empty!!!")
+def Show_tasks():
+    if not ToDo_list:
+        print("Ooh Gosh...The list is empty!!!")
     else:
-        for i, task in enumerate(todo_list, 1):
+        for i, task in enumerate(ToDo_list, 1):
             print(f"{i}. {task}")
 
-def add_task():
+def Add_task():
     task = input("Enter the task title: ")
-    todo_list.append(task)
+    ToDo_list.append(task)
     print("Added task ✅ ")
 
-def remove_task():
-    show_tasks()
+def Remove_task():
+    Show_tasks()
     try:
         index = int(input("The task number you want to delete :"))
-        removed = todo_list.pop(index - 1)
-        print(f" '{removed}' deleted ❌ ")
+        Removed = ToDo_list.pop(index - 1)
+        print(f" '{Removed}' deleted ❌ ")
     except (ValueError, IndexError):
         print("Sorry... Invalid number")
 
 while True:
-    show_menu()
+    Show_menu()
     choice = input("your choice:")
     if choice == "1":
-        show_tasks()
+        Show_tasks()
     elif choice == "2":
-        add_task()
+        Add_task()
     elif choice == "3":
-        remove_task()
+        Remove_task()
     elif choice == "4":
         print("Quit")
         break
